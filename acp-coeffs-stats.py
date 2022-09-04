24d2daa9626a93a52e7c88a8eebddde97c6a2900
mkondratek <mik.kondratek@gmail.com>
2022-09-04 13:45:14 +0200

    Use .TITLE and make plot nicer

--- acp-coeffs-stats.py
+++ acp-coeffs-stats.py
@@ -2,6 +2,7 @@ import csv
 import os
 import sys
 import time
+from os.path import basename
 
 import numpy as np
 
@@ -23,6 +24,9 @@ def main():
     wd = results_path.replace('.', '_').replace('/', '_')
     print(f'Start ({start_time})')
 
+    title = np.array([f for f in os.listdir(results_path) if f.endswith('.TITLE')])[0]
+    title = basename(title).removesuffix('.TITLE')
+
     listdir = np.array([f for f in os.listdir(results_path) if f.endswith('.csv')])
     plt_data = np.zeros((len(listdir)))
     for i, result_file in enumerate(listdir):
@@ -36,20 +40,19 @@ def main():
     all_zeros_idx = np.where(listdir == 'acp-coeffs-all-zeros_txt-stats.csv')
     all_zeros_idx = all_zeros_idx[0][0]
 
-    # only better
-    listdir = listdir[:all_zeros_idx + 1]
-    plt_data = plt_data[:all_zeros_idx + 1]
-
     index_min = min(range(len(plt_data)), key=plt_data.__getitem__)
-    plt.plot(plt_data, listdir, 'k.',
-             label=f'Average {selected_field} by {results_path} from ACP runs with different coefficients')
+    plt.title(f'Average {selected_field} by {results_path} results and {title} dataset\n'
+              f'from ACP runs with different coefficients')
+    plt.plot(plt_data, listdir, 'k.')
     plt.plot(plt_data[all_zeros_idx], listdir[all_zeros_idx], 'ro')
     plt.plot(plt_data[index_min], listdir[index_min], 'go')
 
     plt.annotate(f'{plt_data[all_zeros_idx]:.5f}', (plt_data[all_zeros_idx], listdir[all_zeros_idx]),
-                 textcoords='offset points', xytext=(-25, -12))
+                 textcoords='offset points', xytext=(-100, -10), arrowprops=dict(arrowstyle="->",
+                                                                                 connectionstyle="arc3,rad=.2"))
     plt.annotate(f'{plt_data[index_min]:.5f}', (plt_data[index_min], listdir[index_min]),
-                 textcoords='offset points', xytext=(-20, 5))
+                 textcoords='offset points', xytext=(-5, 50), arrowprops=dict(arrowstyle="->",
+                                                                              connectionstyle="arc3,rad=.2"))
 
     plt.yticks(fontsize=6)
     plt.savefig(f'{wd}-stats.png', bbox_inches='tight', dpi=300)
